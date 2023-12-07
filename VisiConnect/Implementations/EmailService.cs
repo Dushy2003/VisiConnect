c2bbc2a8f0b9c04b619d2abfb4876fe874ed38b0
DushyantBhambore <goodbyebeautiful07@gmail.com>
2023-12-07 22:04:38 +0530

    Add project files.

---
+++ VisiConnect/Implementations/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace VisiConnect.Implementations
+{
+    public class EmailService : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //throw new NotImplementedException();
+            
+        }
+    }
+}
